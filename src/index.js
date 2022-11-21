cce6d590ee73262bbf89e98ab021f92780bd115f
SaraRivAl <sari_ra@hotmail.com>
2022-11-21 11:10:28 +0100

    install starter kit and copy html to app.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+const root = ReactDOM.render(<App />, document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <app />
+  </React.StrictMode>
+);
