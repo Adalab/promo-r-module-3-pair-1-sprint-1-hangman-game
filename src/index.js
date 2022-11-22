1eb49ed1fa28159462e03f419b36140a0bad9cfa
SaraRivAl <sari_ra@hotmail.com>
2022-11-22 11:22:10 +0100

    import scss and image files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-const root = ReactDOM.render(<App />, document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
